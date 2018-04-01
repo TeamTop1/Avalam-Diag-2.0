f5ec2ee8b34098e9d1f215f67e8386c66a1c3f33
Borobo <37670395+Borobo@users.noreply.github.com>
2018-04-01 19:08:20 +0200

    Update maindiag.c switch to if/else
    J'ai changé la methode switch en methode else/if

--- Avalam/libavalam-final/example/maindiag.c
+++ Avalam/libavalam-final/example/maindiag.c
@@ -19,7 +19,7 @@ int main()
 
 	puts("Bienvenue dans Avalam !\n");
 	puts("A vous de jouer !\n");
-	scanf("%s",fen);
+	scanf("%s",&fen);
 	interpretation("diag.json", fen);
 
 	//j/Uu/UuUu/uUuUuU/UuUuUuUu/UuUuuUuU/uUuUuUuU/UuUuUu/uU;
@@ -29,6 +29,8 @@ int interpretation(char * filename, char * tab){
 	FILE * fp;
 	fp = fopen(filename, "w");
 	int i = 0;
+	if(tab[0] == "j") fprintf(fp, "{\n%s:%d,\n",STR_TURN,1);
+	else fprintf(fp, "{\n%s:%d,\n",STR_TURN,1);
 	
 	/*fprintf(fp, "%s:%d,\n",STR_SCORE_J,s.nbJ); 
 	fprintf(fp, "%s:%d,\n",STR_SCORE_J5,s.nbJ5); 
@@ -38,66 +40,55 @@ int interpretation(char * filename, char * tab){
 	fprintf(fp, "%s:[\n", STR_COLS);
 
 	while(tab[i]!="\0"){
-		switch(tab[i])
-		{
-			case "u":
-				fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,1, STR_COULEUR,1);
-				i++;
-				break;
-			case "U":
-				fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,1, STR_COULEUR,2);
-				i++;
-				break;
-			case "d":
-				fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,2, STR_COULEUR,1);
-				i++;
-				break;
-			case "D":
-				fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,2, STR_COULEUR,2);
-				i++;
-				break;
-			case "t":
-				fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,3, STR_COULEUR,1);
-				i++;				
-				break;
-			case "T":
-				fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,3, STR_COULEUR,2);
-				i++;
-				break;
-			case "q":
-				fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,4, STR_COULEUR,1);
-				i++;
-				break;
-			case "Q":
-				fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,4, STR_COULEUR,2);
-				i++;
-				break;
-			case "c":
-				fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,5, STR_COULEUR,1);
-				i++;
-				break;
-			case "C":
-				fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,5, STR_COULEUR,2);
-				i++;
-				break;
-			case "/":
-				i++;
-				break;
-			case ["0","1","2","3","4","5","6","7","8"]:
-				for(int atoi(j) = "0"; j != atoi(tab[i]); j++){
-					fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,0, STR_COULEUR,0);
-				}
-				break;
-			case "j":
-				fprintf(fp, "{\n%s:1,\n",STR_TURN);
-				i++;
-				break;
-			case "r":
-				fprintf(fp, "{\n%s:2,\n",STR_TURN);
-				i++;
-				break;
-			default:
-				break;
+		if(tab[i] = "u"){
+			fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,1, STR_COULEUR,1);
+			i++;
 		}
+		else if(tab[i] ="U"){
+			fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,1, STR_COULEUR,2);
+			i++;
+		}
+		else if(tab[i] ="d"){
+			fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,2, STR_COULEUR,1);
+			i++;
+		}
+		else if(tab[i] ="D"){
+			fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,2, STR_COULEUR,2);
+			i++;
+		}	
+		else if(tab[i] ="t"){
+			fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,3, STR_COULEUR,1);
+			i++;				
+		}	
+		else if(tab[i] ="T"){
+			fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,3, STR_COULEUR,2);
+			i++;
+		}	
+		else if(tab[i] ="q"){
+			fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,4, STR_COULEUR,1);
+			i++;
+		}	
+		else if(tab[i] ="Q"){
+			fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,4, STR_COULEUR,2);
+			i++;
+		}	
+		else if(tab[i] ="c"){
+			fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,5, STR_COULEUR,1);
+			i++;
+		}
+		else if(tab[i] ="C"){
+			fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,5, STR_COULEUR,2);
+			i++;
+		}
+		else if(tab[i] ="/"){
+			i++;
+		}
+			
+		else if((47<tab[i])&&(tab[i]<57)){
+			/*for(int j = atoi("0"); j != atoi(tab[i]); j++){
+				//fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,0, STR_COULEUR,0);
+			}*/
+		}
+	
 	}
 }

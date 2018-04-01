a22b36a1a142968b9ee1568d55a9d872b24dcd32
Borobo <37670395+Borobo@users.noreply.github.com>
2018-04-01 17:47:29 +0200

    Update maindiag.c

--- Avalam/libavalam-final/example/maindiag.c
+++ Avalam/libavalam-final/example/maindiag.c
@@ -84,7 +84,7 @@ int interpretation(char * filename, char * tab){
 			case "/":
 				i++;
 				break;
-			case {"0","1","2","3","4","5","6","7","8"}:
+			case ["0","1","2","3","4","5","6","7","8"]:
 				for(int j = "0"; j != atoi(tab[i]); j++){
 					fprintf(fp, ",\n\t{%s:%d, %s:%d}",STR_NB,0, STR_COULEUR,0);
 				}
